f88f7e0df0133421f958ff1eece938ebb48d1d9e
testcase <testcase>
2006-10-21 21:40:34 +0000

    updated to header versions in src/

--- include/SndObj/SndFIO.h
+++ include/SndObj/SndFIO.h
@@ -22,178 +22,176 @@ enum {OVERWRITE,APPEND,INSERT,READ };
 enum {WAITOPEN=10,SFOPEN,SFERROR};
 
 class SndFIO : public SndIO{
-
-      protected:
-
-      char* m_name;
-      FILE* m_file; 
-	  FILE* m_safe;
-      short m_filestat;
-      short m_mode;
-      float m_spos;
-      long  m_datapos;
-	  long  m_buffsize;  // size of buffer in bytes 
-	  char* m_buffer;
-	  char* m_cp;
-	  short* m_sp;
-	  long* m_lp;
-	  _24Bit* m_s24p;
-	  short m_selfdesc; // self-describing header present
-	  long m_dataframes;
-	  int  m_eof; // end of file indicator
-
-	    // Native <-> Little-endian conversions
-	    long lenlong(long lval);
-        short lenshort(short sval);
-        short natlshort(short sval);
-        long natllong(long lval);
+	
+protected:
+	
+	char* m_name;
+	FILE* m_file; 
+	FILE* m_safe;
+	short m_filestat;
+	short m_mode;
+	float m_spos;
+	long  m_datapos;
+	long  m_buffsize;  // size of buffer in bytes 
+	char* m_buffer;
+	char* m_cp;
+	short* m_sp;
+	long* m_lp;
+	_24Bit* m_s24p;
+	short m_selfdesc; // self-describing header present
+	long m_dataframes;
+	int  m_eof; // end of file indicator
+	
+	short m_itemsleft;
+	
+	/* Byte Swapping Functions */
+	
+	short SwapShort(short inv);
+	long SwapLong(long inv);
+	float SwapFloat32( float inv );
+	double SwapDouble( double inv );
+	
+public:
 		
-		// Native <->  Big-endian conversions
-		long benlong(long lval);
-        short benshort(short sval);
-        short natshort(short sval);
-        long natlong(long lval);   
-		short m_itemsleft;
-
-      public:
-	     
-		  FILE *GetFile(){return m_safe; }
-      short GetMode(){ return m_mode; }
-      void SetPos(float pos){ 
-          m_spos = pos;
-	      if(!fseek(m_safe,(long)(m_spos*m_sr*m_channels*(m_bits/8))+
-                       m_datapos,
-		 SEEK_SET)) m_eof=0; 
-                  }
-	  void SetPos(long pos){
+		FILE *GetFile(){return m_safe; }
+	short GetMode(){ return m_mode; }
+	void SetPos(float pos){ 
+		m_spos = pos;
+		if(!fseek(m_safe,(long)(m_spos*m_sr*m_channels*(m_bits/8))+
+				  m_datapos,
+				  SEEK_SET)) m_eof=0; 
+	}
+	void SetPos(long pos){
        	if(!fseek(m_safe, pos  +
-			m_datapos, SEEK_SET)){
+				  m_datapos, SEEK_SET)){
 			if(m_sr && m_channels && m_bits)
-			m_spos = pos/(m_sr*m_channels*(m_bits/8));
+				m_spos = pos/(m_sr*m_channels*(m_bits/8));
 			m_eof=0; 
 		}
-	  }
-           
-	  int  Eof() { return (m_eof = feof(m_file));}
-	  long  GetDataFrames(){ return m_dataframes;} 
-      float GetPos(){ return m_spos; }
-      short GetStatus(){ return m_filestat; }
+	}
+	
+	int  Eof() { return (m_eof = feof(m_file));}
+	long  GetDataFrames(){ return m_dataframes;} 
+	float GetPos(){ return m_spos; }
+	short GetStatus(){ return m_filestat; }
+	
+	SndFIO(char* name, short mode, short channels=1, short bits=16,
+		   SndObj** inputlist=0, float spos= 0.f,
+		   int vecsize=DEF_VECSIZE, float sr=DEF_SR);
+	~SndFIO();
+	short Read();
+	short Write();
+	char* ErrorMessage();
+	
+};
 
-	  SndFIO(char* name, short mode, short channels=1, short bits=16,
-		     SndObj** inputlist=0, float spos= 0.f,
-			  int vecsize=DEF_VECSIZE, float sr=DEF_SR);
-	  ~SndFIO();
-	  short Read();
-	  short Write();
-	  char* ErrorMessage();
 
-                          };
-
-short inline
-SndFIO::lenshort(short sval) 
+inline short SndFIO::SwapShort (short inv)
 {
-    char  benchar[2];
-    register char *p = benchar;
-
-    *p++ = 0xFF & sval;
-    *p   = 0xFF & (sval >> 8);
-    return(*(short *)benchar);
+	union shortconv {
+		short	us;
+		unsigned char uc[2];
+	} *inp, outv;
+	
+	inp = (union shortconv *)&inv;
+	
+	outv.uc[0] = inp->uc[1];
+	outv.uc[1] = inp->uc[0];
+	
+	return( outv.us);
 }
 
-long inline
-SndFIO::lenlong(long lval)
+inline long SndFIO::SwapLong (long inv)
 {
-    char  benchar[4];
-    register char *p = benchar;
-
-    *p++ = (char) (0xFF & lval);
-    *p++ = (char) (0xFF & (lval >> 8));
-    *p++ = (char) (0xFF & (lval >> 16));
-    *p   = (char) (0xFF & (lval >> 24));
-    return(*(long *)benchar);
+	union longconv {
+		long	ul;
+		unsigned char uc[4];
+	} *inp, outv;
+	
+	inp = (union longconv *)&inv;
+	
+	outv.uc[0] = inp->uc[3];
+	outv.uc[1] = inp->uc[2];
+	outv.uc[2] = inp->uc[1];
+	outv.uc[3] = inp->uc[0];
+	
+	return( outv.ul);
+	
 }
 
-short inline
-SndFIO::natlshort(short sval) 
+inline float SndFIO::SwapFloat32( float inv )
 {
-    unsigned char benchar[2];
-    register short natshort;
-
-    *(short *)benchar = sval;
-    natshort = benchar[1];
-    natshort <<= 8;
-    natshort |= benchar[0];
-    return(natshort);
+    union floatconv {
+		float				uf;
+		unsigned char		uc[4];
+    } *inp, outv;
+    
+    inp = (union floatconv *)&inv;
+    
+    outv.uc[0] = inp->uc[3];
+    outv.uc[1] = inp->uc[2];
+    outv.uc[2] = inp->uc[1];
+    outv.uc[3] = inp->uc[0];
+    
+    return( outv.uf );
 }
 
-long inline
-SndFIO::natllong(long lval)     
+inline double SndFIO::SwapDouble( double inv )
 {
-    unsigned char benchar[4];
-    register unsigned char *p = benchar + 3;
-    register long natlong;
-
-    *(long *)benchar = lval;
-    natlong = *p--;
-    natlong <<= 8;
-    natlong |= *p--;
-    natlong <<= 8;
-    natlong |= *p--;
-    natlong <<= 8;
-    natlong |= *p;
-    return(natlong);
+    union doubleconv {
+		double				ud;
+		unsigned char		uc[8];
+    } *inp, outv;
+    
+    inp = (union doubleconv *)&inv;
+    
+    outv.uc[0] = inp->uc[7];
+    outv.uc[1] = inp->uc[6];
+    outv.uc[2] = inp->uc[5];
+    outv.uc[3] = inp->uc[4];
+    outv.uc[4] = inp->uc[3];
+    outv.uc[5] = inp->uc[2];
+    outv.uc[6] = inp->uc[1];
+    outv.uc[7] = inp->uc[0];
+    
+    return( outv.ud );
 }
 
-short inline
-SndFIO::benshort(short sval)   // coerce a natural short into a bigendian short 
-{
-    char  benchar[2];
-    register char *p = benchar;
-    *p++ = 0xFF & (sval >> 8);
-    *p   = 0xFF & sval;
-    return(*(short *)benchar);
-}
+/* Maybe not the best way, but these deal with endian issues
+for any type of io use the macro around what type is expected
+to be written or read. if this is already the processors native form 
+it will do nothing, if not it will swap bytes.
 
-long inline 
-SndFIO::benlong(long lval)       // coerce a natural long into a bigendian long 
-{
-    char  benchar[4];
-    register char *p = benchar;
-    *p++ = (char) (0xFF & (lval >> 24));
-    *p++ = (char) (0xFF & (lval >> 16));
-    *p++ = (char) (0xFF & (lval >> 8));
-    *p   = (char) (0xFF & lval);
-    return(*(long *)benchar);
-}
+so, when reading or writing a LE type on a BE machine, SHORT_LE( n ) 
+will swap bytes for reading and writing. when reading or writing a LE
+type on a LE machine, it does nothing
+*/
 
-short inline 
-SndFIO::natshort(short sval)    // coerce a bigendian short into a natural short 
-{
-    unsigned char benchar[2];
-    register short natshort;
-    *(short *)benchar = sval;
-    natshort = benchar[0];
-    natshort <<= 8;
-    natshort |= benchar[1];
-    return(natshort);
-}
+#ifdef WORDS_BIGENDIAN
 
-long inline 
-SndFIO::natlong(long lval)      // coerce a bigendian long into a natural long 
-{
-    unsigned char benchar[4];
-    register unsigned char *p = benchar;
-    register long natlong;
-    *(long *)benchar = lval;
-    natlong = *p++;
-    natlong <<= 8;
-    natlong |= *p++;
-    natlong <<= 8;
-    natlong |= *p++;
-    natlong <<= 8;
-    natlong |= *p;
-    return(natlong);
-}
+#define SHORT_LE(x) SwapShort(x)
+#define LONG_LE(x) SwapLong(x)
+#define FLOAT32_LE(x) SwapFloat32(x)
+#define DOUBLE_LE(x) SwapDouble(x)
+#define SHORT_BE(x) x
+#define LONG_BE(x) x
+#define FLOAT32_BE(x) x
+#define DOUBLE_BE(x) x
+
+
+#else
+
+#define SHORT_LE(x) x
+#define LONG_LE(x) x
+#define FLOAT32_LE(x) x
+#define DOUBLE_LE(x) x
+#define SHORT_BE(x) SwapShort(x)
+#define LONG_BE(x) SwapLong(x)
+#define FLOAT32_BE(x) SwapFloat32(x)
+#define DOUBLE_BE(x) SwapDouble(x)
+
+
+#endif
 
 #endif
 
