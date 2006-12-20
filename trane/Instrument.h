67ef692cc86f5b34857257b4a943595511501394
veplaini <veplaini>
2006-12-20 08:19:27 +0000

    add trane srcs

--- trane/Instrument.h
+++ trane/Instrument.h
@@ -1,56 +1,91 @@
-// Instrument.h: interface for the Instrument class.
-//
-//////////////////////////////////////////////////////////////////////
+
 #ifndef INSTRUMENT_H
 #define INSTRUMENT_H
-
-
 #include "SndObj/AudioDefs.h"
 
 class Instrument  
 {
-	
-	float res[4];
-	float fr[4];
-	float gloop[4];
-	float gstr[4];
+  float res[4];
+  float fr[4];
+  float gloop[4];
+  float gstr[4];
  
-	bool process_on;
+  bool process_on;
 
-	// SndObjs
-        StringFlt *string[4];
-	Gain      *strgain[4];
-	Gain    *loopgain[4];
-	Mixer   *strmix;
-	Mixer   *loopmix;
-        SndIn   *sound;
-	SndLoop *loop[4];
-	Allpass *ap[2];
-        SndIO   *input;
-	SndIO   *output;
-	SndObj  *outchannel[2];
-	SndThread *thread;
+  // SndObjs
+  StringFlt *string[4];
+  Gain      *strgain[4];
+  Gain    *loopgain[4];
+  Mixer   *strmix;
+  Mixer   *loopmix;
+  SndIn   *sound;
+  SndLoop *loop[4];
+  Allpass *ap[2];
+  SndIO   *input;
+  SndIO   *output;
+  SndObj  *outchannel[2];
+  SndThread *thread;
 
-	public:
+ public:
 
-	void ReSample(int no);
-	void LoopOff(int no);
-	void LoopOn(int no);
-	void StringOff(int no);
-	void StringOn(int no);
-	void SetPitch(int no, float pitch);
-	void SetLoopGain(int no, float gain);
-	void SetStrGain(int no, float gain);
-	void SetRes(int no, float res);
-	void SetFreq(int no, float freq);
-	void ProcessOff();
-	void ProcessOn();
-        float GetFreq(int no) { if(no<4 && no >= 0)return fr[no]; }
-	float GetRes(int no){ if(no<4 && no >= 0)return res[no]; }
-        float GetLoopGain(int no) { if(no<4 && no >= 0)return gloop[no]; }
-	float GetStrGain(int no){ if(no<4 && no >= 0)return gstr[no]; }
-	Instrument();
-	virtual ~Instrument();
+  void ReSample(int no){
+    if(no >= 0 && no < 4) loop[no]->ReSample();
+  }
+  void LoopOff(int no){
+    if(no >= 0 && no < 4){
+      loopgain[no]->Disable();
+      loop[no]->Disable();
+    }
+  }
+  void LoopOn(int no){
+    if(no >= 0 && no < 4){
+      loopgain[no]->Enable();
+      loop[no]->Enable();
+    }
+  }
+  void StringOff(int no){
+    if(no >= 0 && no < 4){
+      strgain[no]->Disable();
+      string[no]->Disable();
+    }
+  }
+  void StringOn(int no){
+    if(no >= 0 && no < 4){
+      strgain[no]->Enable();
+      string[no]->Enable();
+    }
+  }
+  void SetPitch(int no, float pitch){
+    if(no >= 0 && no < 4) loop[no]->SetPitch(pitch);
+  }
+  void SetLoopGain(int no, float gain){
+    if(no >= 0 && no < 4) loopgain[no]->SetGain(gain);
+  }
+  void SetStrGain(int no, float gain){
+    if(no >= 0 && no < 4) strgain[no]->SetGain(gain);
+  }
+  void SetRes(int no, float res){
+    if(no >= 0 && no < 4) string[no]->SetFdbgain(res);
+  }
+  void SetFreq(int no, float freq){
+    if(no >= 0 && no < 4) string[no]->SetFreq(freq);
+  }
+  float GetFreq(int no){ 
+    if(no<4 && no >= 0) return fr[no]; 
+  }
+  float GetRes(int no){ 
+    if(no<4 && no >= 0) return res[no]; 
+  }
+  float GetLoopGain(int no){ 
+    if(no<4 && no >= 0) return gloop[no]; 
+  }
+  float GetStrGain(int no){ 
+    if(no<4 && no >= 0) return gstr[no]; 
+  }
+  void ProcessOff();
+  void ProcessOn();
+  Instrument();
+  virtual ~Instrument();
 
 };
 
--- trane/main.cpp
+++ trane/main.cpp
@@ -9,152 +9,134 @@
 #include "Instrument.h"
 
 struct InstData {
-   Instrument trane;
-   Fl_Value_Slider *str_fr[4], *loop_gain[4];
-   Fl_Dial *str_gain[4];
-   Fl_Light_Button *str_bt[4], *loop_bt[4];
-   Fl_Button *resamp_bt[4];
-
+  Instrument trane;
+  Fl_Value_Slider *str_fr[4], *loop_gain[4];
+  Fl_Dial *str_gain[4];
+  Fl_Light_Button *str_bt[4], *loop_bt[4];
+  Fl_Button *resamp_bt[4];
 };
 
 static void fr_callback(Fl_Widget* o, void* d) {
-
-	InstData *p = (InstData *) d;
-    for(int i=0; i < 4; i++)
-	if(o == p->str_fr[i]){
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->str_fr[i]){
       p->trane.SetFreq(i,((Fl_Value_Slider *)o)->value());
-	}
-
+    }
 }
 
 static void str_callback(Fl_Widget* o, void* d) {
 
-InstData *p = (InstData *) d;
-for(int i=0; i < 4; i++)
-	if(o == p->str_bt[i]){
-	  if(((Fl_Button *)o)->value())
-      p->trane.StringOn(i);
-	  else
-      p->trane.StringOff(i);
-	}
-
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->str_bt[i]){
+      if(((Fl_Button *)o)->value())
+	p->trane.StringOn(i);
+      else
+	p->trane.StringOff(i);
+    }
 }
 
 static void loop_callback(Fl_Widget* o, void* d) {
-
-   InstData *p = (InstData *) d;
-   for(int i=0; i < 4; i++)
-	if(o == p->loop_bt[i]){
-	  if(((Fl_Button *)o)->value())
-      p->trane.LoopOn(i);
-	  else
-      p->trane.LoopOff(i);
-	}
-
-
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->loop_bt[i]){
+      if(((Fl_Button *)o)->value())
+	p->trane.LoopOn(i);
+      else
+	p->trane.LoopOff(i);
+    }
 }
 
 static void resample_callback(Fl_Widget* o, void *d) {
 
-   InstData *p = (InstData *) d;
-   for(int i=0; i < 4; i++)
-	if(o == p->resamp_bt[i]){
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->resamp_bt[i]){
       p->trane.ReSample(i);
-	}
-
-
+    }
 }
 
 static void sgain_callback(Fl_Widget* o, void *d) {
-
-	InstData *p = (InstData *) d;
-    for(int i=0; i < 4; i++)
-	if(o == p->str_gain[i]){
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->str_gain[i]){
       p->trane.SetStrGain(i,((Fl_Dial *)o)->value());
-	}
+    }
 }
 
 static void lgain_callback(Fl_Widget* o, void *d) {
-
-	InstData *p = (InstData *) d;
-    for(int i=0; i < 4; i++)
-	if(o == p->loop_gain[i]){
+  InstData *p = (InstData *) d;
+  for(int i=0; i < 4; i++)
+    if(o == p->loop_gain[i]){
       p->trane.SetLoopGain(i,((Fl_Value_Slider *)o)->value());
-	}
+    }
 }
 
 int main(int argc, char **argv) {
 
- InstData  inst; 
- int red = fl_rgb_color(200,10,40);
- int redish = fl_rgb_color(180, 60, 60);
- Fl_Window *window = new Fl_Window(460,660, "The Trane Thing Computer Instrument");
- window->color(FL_DARK_BLUE);
- Fl_Group *strings = new Fl_Group(5,15,450,245);
- strings->box(FL_ENGRAVED_BOX);
- strings->color(red);
- strings->end();
- Fl_Group *loops = new Fl_Group(5,270,450,385);
- loops->box(FL_ENGRAVED_BOX);
- loops->color(red);
- loops->end();
- 
+  InstData  inst; 
+  char mess1[4][10], mess2[4][10];
+  int red = fl_rgb_color(200,10,40);
+  int redish = fl_rgb_color(180, 60, 60);
+  Fl_Window *window = new Fl_Window(460,660, "The Trane Thing Computer Instrument");
+  window->color(FL_DARK_BLUE);
+  Fl_Group *strings = new Fl_Group(5,15,450,245);
+  strings->box(FL_ENGRAVED_BOX);
+  strings->color(red);
+  strings->end();
+  Fl_Group *loops = new Fl_Group(5,270,450,385);
+  loops->box(FL_ENGRAVED_BOX);
+  loops->color(red);
+  loops->end();
 
-
- char mess1[4][10], mess2[4][10];
-
- for(int i=0; i < 4; i++) {
- 
- sprintf(mess1[i], "string %d", i+1);
- // strings
- inst.str_bt[i] = new Fl_Light_Button(15,25+i*60, 70, 30, mess1[i]);
- inst.str_bt[i]->type(FL_TOGGLE_BUTTON);
- inst.str_bt[i]->color(redish);
- inst.str_bt[i]->callback((Fl_Callback*)str_callback, (void *) &inst);
- strings->add(inst.str_bt[i]);
- if(i==3)
- inst.str_fr[i] = new Fl_Value_Slider(105, 20+i*60, 280, 40, "frequency");
- else
- inst.str_fr[i] = new Fl_Value_Slider(105, 20+i*60, 280, 40);
- inst.str_fr[i]->range(inst.trane.GetFreq(i),inst.trane.GetFreq(i)*4);
- inst.str_fr[i]->type(5);
- inst.str_fr[i]->callback((Fl_Callback*)fr_callback, (void *)&inst);
- inst.str_fr[i]->color(red);
- inst.str_fr[i]->box(FL_FLAT_BOX);
- strings->add(inst.str_fr[i]);
-if(i==3)
- inst.str_gain[i] = new Fl_Dial(405, 20+i*60, 40, 40, "Gain");
-else
-inst.str_gain[i] = new Fl_Dial(405, 20+i*60, 40, 40);
- inst.str_gain[i]->callback((Fl_Callback*)sgain_callback, (void *)&inst);
- inst.str_gain[i]->range(-60,0);
- inst.str_gain[i]->box(FL_ROUND_UP_BOX);
- //inst.str_gain[i]->color(fl_rgb_color(140, 60, 60));
- strings->add(inst.str_gain[i]);
- // loops
- sprintf(mess2[i], "sample %d", i+1);
- inst.loop_bt[i] = new Fl_Light_Button(15+i*115,280, 80, 30, mess2[i]);
- inst.loop_bt[i]->type(FL_TOGGLE_BUTTON);
- inst.loop_bt[i]->color(redish);
- inst.loop_bt[i]->callback((Fl_Callback*)loop_callback, (void *) &inst);
- loops->add(inst.loop_bt[i]);
- inst.loop_gain[i] = new Fl_Value_Slider(35+i*115,330, 40, 250, "gain(dB)");
- inst.loop_gain[i]->range(24,-24);
- inst.loop_gain[i]->color(redish);
- inst.loop_gain[i]->type(FL_VERT_NICE_SLIDER);
- inst.loop_gain[i]->callback((Fl_Callback*)lgain_callback, (void *)&inst);
- loops->add(inst.loop_gain[i]);
- inst.resamp_bt[i] = new Fl_Button(15+i*115,600, 80, 30, "resample");
- inst.resamp_bt[i]->box(FL_ROUND_UP_BOX);
- //inst.resamp_bt[i]->color(redish);
- inst.resamp_bt[i]->type(0);
- inst.resamp_bt[i]->callback((Fl_Callback*)resample_callback, (void *) &inst);
- loops->add(inst.resamp_bt[i]);
- }
- window->end();
- window->show();
- inst.trane.ProcessOn();
- Fl::run();
-
-return 0;
+  for(int i=0; i < 4; i++) {
+    // strings
+    sprintf(mess1[i], "string %d", i+1);
+    inst.str_bt[i] = new Fl_Light_Button(15,25+i*60, 70, 30, mess1[i]);
+    inst.str_bt[i]->type(FL_TOGGLE_BUTTON);
+    inst.str_bt[i]->color(redish);
+    inst.str_bt[i]->callback((Fl_Callback*)str_callback, (void *) &inst);
+    strings->add(inst.str_bt[i]);
+    if(i==3)
+      inst.str_fr[i] = new Fl_Value_Slider(105, 20+i*60, 280, 40, "frequency");
+    else
+      inst.str_fr[i] = new Fl_Value_Slider(105, 20+i*60, 280, 40);
+    inst.str_fr[i]->range(inst.trane.GetFreq(i),inst.trane.GetFreq(i)*4);
+    inst.str_fr[i]->type(5);
+    inst.str_fr[i]->callback((Fl_Callback*)fr_callback, (void *)&inst);
+    inst.str_fr[i]->color(red);
+    inst.str_fr[i]->box(FL_FLAT_BOX);
+    strings->add(inst.str_fr[i]);
+    if(i==3)
+      inst.str_gain[i] = new Fl_Dial(405, 20+i*60, 40, 40, "Gain");
+    else
+      inst.str_gain[i] = new Fl_Dial(405, 20+i*60, 40, 40);
+    inst.str_gain[i]->callback((Fl_Callback*)sgain_callback, (void *)&inst);
+    inst.str_gain[i]->range(-60,0);
+    inst.str_gain[i]->box(FL_ROUND_UP_BOX);
+    strings->add(inst.str_gain[i]);
+    // loops
+    sprintf(mess2[i], "sample %d", i+1);
+    inst.loop_bt[i] = new Fl_Light_Button(15+i*115,280, 80, 30, mess2[i]);
+    inst.loop_bt[i]->type(FL_TOGGLE_BUTTON);
+    inst.loop_bt[i]->color(redish);
+    inst.loop_bt[i]->callback((Fl_Callback*)loop_callback, (void *) &inst);
+    loops->add(inst.loop_bt[i]);
+    inst.loop_gain[i] = new Fl_Value_Slider(35+i*115,330, 40, 250, "gain(dB)");
+    inst.loop_gain[i]->range(24,-24);
+    inst.loop_gain[i]->color(redish);
+    inst.loop_gain[i]->type(FL_VERT_NICE_SLIDER);
+    inst.loop_gain[i]->callback((Fl_Callback*)lgain_callback, (void *)&inst);
+    loops->add(inst.loop_gain[i]);
+    inst.resamp_bt[i] = new Fl_Button(15+i*115,600, 80, 30, "resample");
+    inst.resamp_bt[i]->box(FL_ROUND_UP_BOX);
+    inst.resamp_bt[i]->type(0);
+    inst.resamp_bt[i]->callback((Fl_Callback*)resample_callback, (void *) &inst);
+    loops->add(inst.resamp_bt[i]);
+  }
+  window->end();
+  window->show();
+  inst.trane.ProcessOn();
+  Fl::run();
+  return 0;
 }
