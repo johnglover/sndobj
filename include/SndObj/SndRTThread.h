e9ff806fc407b23f55d08ac8251ec7776327a982
veplaini <veplaini>
2007-01-18 19:13:00 +0000

    added SndRTThread class

---
+++ include/SndObj/SndRTThread.h
@@ -0,0 +1,58 @@
+// Copyright (c)Victor Lazzarini and Rory Walsh, 1997-2006
+// See License.txt for a disclaimer of all warranties
+// and licensing information
+
+//
+// SndRTThread.h 
+// Class SndRTThread: 
+// Multithreading support for the SndObj Library
+// with Realtime audio IO
+// Victor Lazzarini, 2001
+// Win32 thread code by Rory Walsh, 2006
+
+#ifndef _SNDRTTHREAD_H
+#define _SNDRTTHREAD_H
+
+#include "SndThread.h"
+#include "Mix.h"
+#include "SndIn.h"
+
+class SndRTThread : public SndThread {
+
+  SndIO *in; 
+  SndIO *out;
+  Mixer **ochannel;
+  SndIn **sound;
+  int m_channels;
+  int m_buffsize;
+  int m_periods;
+
+  void Init();
+
+ public:
+ 
+  SndRTThread();
+  SndRTThread(int channels, int buffsize=1024, int periods=4);
+  SndRTThread(int n, SndObj** objlist, int channels=2, int buffsize=1024, int periods=4);
+  ~SndRTThread();
+
+  void AddOutput(int channel, SndObj *in){
+    AddObj(in);
+    ochannel[channel]->AddObj(in);
+  }
+  void DeleteOutput(int channel, SndObj *in){
+    DeleteObj(in);
+    ochannel[channel]->DeleteObj(in);
+  }
+
+  SndObj *GetInput(int channel) { return sound[channel]; }   
+
+  friend void rtthreadfunc(void *);
+};
+
+void rtthreadfunc(void *);
+
+#endif         
+
+
+
---
+++ src/SndRTThread.cpp
@@ -0,0 +1,142 @@
+#include "SndRTThread.h"
+#include "SndRTIO.h"
+#include "SndCoreAudio.h"
+
+SndRTThread::SndRTThread() {
+  m_channels = 2;
+#ifdef MACOSX
+  in = new SndCoreAudio(m_channels);
+  out = in;  
+#else
+  in = new SndRTIO(m_channels, SND_INPUT);
+  out = new SndRTIO(m_channels, SND_OUTPUT);
+#endif
+  sound = new SndIn*[m_channels];
+  sound[0] = new SndIn(in, 1);
+  sound[1] = new SndIn(in, 2);
+
+  ochannel = new Mixer*[m_channels];
+  ochannel[0] = new Mixer;
+  ochannel[1] = new Mixer;
+
+  out->SetOutput(1, ochannel[0]);
+  out->SetOutput(2, ochannel[1]);
+
+  AddObj(in, SNDIO_IN);
+  AddObj(out, SNDIO_OUT);
+
+  SndProcessThread = rtthreadfunc;
+}
+
+SndRTThread::SndRTThread(int channels, 
+       int buffsize, int periods) :
+  SndThread(){
+
+  m_channels = channels;
+  m_buffsize = buffsize;
+  m_periods = periods;
+  Init();
+}
+
+SndRTThread::SndRTThread(int n, SndObj** objlist, int channels, 
+       int buffsize, int periods) :
+  SndThread(n,objlist,0){
+
+  m_channels = channels;
+  m_buffsize = buffsize;
+  m_periods = periods;
+  Init();
+}
+
+void SndRTThread::Init(){
+  int i;
+#ifdef MACOSX
+  in = new SndCoreAudio(m_channels,m_buffsize,m_periods);
+  out = in;  
+#else
+  in = new SndRTIO(m_channels, SND_INPUT, m_buffsize,m_periods);
+  out = new SndRTIO(m_channels, SND_OUTPUT, m_buffsize,m_periods);
+#endif
+  sound = new SndIn*[m_channels];
+
+  for(i=0; i < m_channels; i++)
+         sound[i] = new SndIn(in, i+1);
+
+  ochannel = new Mixer*[m_channels];
+
+  for(i=0; i < m_channels; i++){
+         ochannel[i] = new Mixer();
+         out->SetOutput(i+1, ochannel[i]);
+  }
+  AddObj(in, SNDIO_IN);
+  AddObj(out, SNDIO_OUT);
+
+  SndProcessThread = rtthreadfunc;
+}
+
+SndRTThread::~SndRTThread(){
+  delete[] ochannel;
+  delete[] sound;
+  delete in;
+  delete out;
+}
+
+
+void
+rtthreadfunc(void* data){
+    
+  int i, channels, objno, inputno, outputno;
+  SndRTThread *sndthread = (SndRTThread *) data;
+  SndLink<SndObj>* temp; 
+  SndLink<SndIO>* itemp;
+  SndLink<SndIO>* otemp; 
+  channels =  sndthread->m_channels;
+  SndIn **sound = sndthread->sound;
+  Mixer **ochannel = sndthread->ochannel;
+
+  while(sndthread->status){
+ 
+    //... processing loop...
+   temp = sndthread->last;
+   itemp = sndthread->input;
+   otemp = sndthread->output;
+
+    sndthread->Update();
+    
+    for(i = 0; i < sndthread->InputNo; i++){
+      itemp->obj->Read();
+      itemp = itemp->next;
+    }
+    // callback
+    if(sndthread->ProcessCallback != NULL)
+    sndthread->ProcessCallback(sndthread->callbackdata);          
+
+    // sound processing 
+    // inputs
+    for(i = 0; i < channels; i++)
+      sound[i]->DoProcess();
+
+    sndthread->processing = true;
+    for(i = 0; i < sndthread->SndObjNo; i++){
+      temp = temp->next;
+      temp->obj->DoProcess();
+    } 
+    sndthread->processing = false;
+ 
+    // outputs
+    for(i = 0; i < channels; i++)
+      ochannel[i]->DoProcess();
+
+    // output processing   
+    for(i = 0; i < sndthread->OutputNo; i++){ 
+      otemp->obj->Write();
+      otemp = otemp->next;
+		
+    }
+  }
+#ifndef USE_WIN32THREADS 
+  return;
+#else
+  _endthread();
+#endif
+}
